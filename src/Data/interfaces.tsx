9099fca3ebef8ac144f7eb7157e5534e224a081d
Diego <marcanp.diego@gmail.com>
2020-07-18 10:54:10 -0400

    scaffolding interfaces as well

---
+++ src/Data/interfaces.tsx
@@ -0,0 +1,17 @@
+/**
+ * This namespace
+ * holds all the interfaces for the data used throughout the apps
+ * 
+ * */ 
+
+namespace DataInterface {
+
+    interface HeroData { 
+
+    }
+
+    interface ProjectData { 
+
+    }
+
+}
