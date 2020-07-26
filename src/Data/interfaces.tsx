382822dcdc05f1da22c8fd2c156a6814f9451a3b
Dmarcano <dmarcan1@swarthmore.edu>
2020-07-26 12:18:57 -0400

    updating project interface

--- src/Data/interfaces.tsx
+++ src/Data/interfaces.tsx
@@ -1,11 +1,13 @@
 /**
  * This namespace
- * holds all the interfaces for the data used throughout the apps
+ * holds all the interfaces for the data used throughout the app
  * 
  * */ 
 
 namespace DataInterface {
 
+    // This interface holds all the information necessary to build the APP, 
+    // it is a collection of other interfaces which the app relies on
     export interface AppData {
         hero_data : HeroData
         project_section_data : ProjectSection
@@ -28,9 +30,17 @@ namespace DataInterface {
         data : Array<ProjectData>
     }
     export interface ProjectData {
-          image ? : ImageData,
-          title ? : string, 
-          description ? : string, 
+          image  : ImageData,
+          title  : string, 
+          description  : string, 
+          id  : number ,
+          github_url : string,  
+          demo_url ? : string, 
+          technologies ? : Array<Technology> 
+    }
+
+    export interface Technology { 
+
     }
 
     interface ExperienceData { 
