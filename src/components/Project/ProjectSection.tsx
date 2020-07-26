42c93fe4b03090d4f524cdb335a928fb4dd77b8e
Dmarcano <dmarcan1@swarthmore.edu>
2020-07-26 12:15:22 -0400

    set up Projects Section

--- src/components/Project/ProjectSection.tsx
+++ src/components/Project/ProjectSection.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import DataInterface from '../../Data/interfaces'
-
+import { Container, Row, Col } from 'react-bootstrap';
+import Title from '../Title/Title'
 
 const ProjectSection = (project_section_data : DataInterface.ProjectSection) => {
-    return <div></div>
+    return (
+    <section id = "projects">
+        <Container>
+            <div className="project-wrapper">
+                <Title title = "Projects"></Title>
+            </div>
+        </Container>
+    </section>
+    )
 }
 
 export default ProjectSection;
